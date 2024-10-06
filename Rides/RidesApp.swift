7f5a6391a82b1b756de42349decebc1d867e4041
Ferrakkem <ferrakkem@gmail.com>
2024-10-06 16:31:20 -0300

    Initial Commit

---
+++ Rides/RidesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RidesApp.swift
+//  Rides
+//
+//  Created by Ferrakkem Bhuiyan on 2024-10-06.
+//
+
+import SwiftUI
+
+@main
+struct RidesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RidesTests/RidesTests.swift
@@ -0,0 +1,17 @@
+//
+//  RidesTests.swift
+//  RidesTests
+//
+//  Created by Ferrakkem Bhuiyan on 2024-10-06.
+//
+
+import Testing
+@testable import Rides
+
+struct RidesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
